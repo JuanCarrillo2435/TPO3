76ec395965abe0f41ba7a95b6d3e2da4f967fe9a
JuanCarrillo2435 <juancarrillo.dc@gmail.com>
2023-08-18 17:03:57 -0300

    TP3 de lab1

---
+++ TPO3Lab1/src/tpo3lab1/TPO3Lab1.java
@@ -0,0 +1,7 @@
+package tpo3lab1;
+import java.util.Scanner;
+public class TPO3Lab1 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    }
+}
